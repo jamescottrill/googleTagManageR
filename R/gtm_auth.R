d3e061992c58a8d8666b6213f398076b1e553215
jamescottrill <james@jamescottrill.co.uk>
2020-07-03 11:48:45 +0100

    Added email parameter to auth documentation

--- R/gtm_auth.R
+++ R/gtm_auth.R
@@ -4,20 +4,31 @@
 #' @family login functions
 #' 
 #' @param token An existing Google Auth Token
+#' @param email The email address for the Google Account
 #' 
 #' @description 
 #' This function authenticates the user with Google Tag Manager
-#' 
-#' 
+#' To use your own GTM API credentials, either ensure that you have set the
+#' path to your OAuth2 API Credentials file in your 
+#' 'GAR_CLIENT_JSON' variable in your .Renviron file and then restart your 
+#' R session or set 
+#'  
+#'
+#' @examples 
+#'  
+#'  \dontrun{
+#'  gtm_auth()
+#'  
+#'  gtm_auth(email="me@mycompany.co.uk")
+#'  }
+#'  
+#' @return Invisibly, the token that has been saved to the session
 #' @export
-gtm_auth<-function(token = NULL){
-  
-  if(!is.null(token)){
-    return(googleAuthR::gar_auth(token = token))
-  }
-  
+gtm_auth<-function(email = NULL, token = NULL){
+
   default_project_message()
-  out<-googleAuthR::gar_auth(token = token,
+  out<-googleAuthR::gar_auth(email = email,
+                             token = token,
                              package = "googleTagManageR")
   myMessage("Authenticated", level = 3)
   invisible(out)
